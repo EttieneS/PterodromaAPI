6149851622655cc1cd7108a806f6dd69bb642ed6
Ettiene <smithettiene@yahoo.com>
2022-06-29 20:40:58 +0200

    currency, selection models: selectioncontroller proto

---
+++ Controllers/SelectionController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ASGExchangeAPI.Models;
+
+namespace ASGExchangeAPI.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SelectionController : ControllerBase
+    {
+        [Route("add")]
+        [HttpPost]
+        public long Add(Currency[] model)
+        {            
+            //SelectionRepository selectionRepository = new SelectionRepository(_context);
+            
+            //Selection selection = new Selection();
+
+            //ong selectionId = selectionRepository.Add(selection);
+
+            long selectionId = 1;
+            return selectionId;
+        }
+
+
+    }
+}
---
+++ Models/Currency.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ASGExchangeAPI.Models
+{
+    public class Currency
+    {
+        public long Id { get; set; }
+        public string? Name { get; set; }
+        public string? Code { get; set; }
+    }
+}
